955fbff2b00891a18ba62289ad425ffc58dfdc9f
Kei-141 <ruler141@gmail.com>
2021-09-28 00:16:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import matplotlib
 from hparams import create_hparams
 from model import Tacotron2
 from train import load_model
@@ -8,9 +9,9 @@ from denoiser import Denoiser
 
 class MikochiSpeechSynthesis:
     def __init__(self):
-        self.hparams = set_hparams()
-        self.model = set_model()
-        self.waveglow = set_waveglow()
+        self.hparams = self.set_hparams()
+        self.model = self.set_model()
+        self.waveglow = self.set_waveglow()
         
     def set_hparams(self):
         hparams = create_hparams()
@@ -22,7 +23,7 @@ class MikochiSpeechSynthesis:
         model = load_model(self.hparams)
         model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
         _ = model.cuda().eval().half()
-        return 
+        return model
 
     def set_waveglow(self):
         waveglow_path = 'waveglow_256channels_universal_v5.pt'
@@ -41,5 +42,4 @@ class MikochiSpeechSynthesis:
         with torch.no_grad():
             audio = self.waveglow.infer(mel_outputs_postnet, sigma=0.666)
         wav = audio[0].data.cpu().numpy()
-        fs = self.hparams.sampling_rate
-        return wav, fs
+        return wav
