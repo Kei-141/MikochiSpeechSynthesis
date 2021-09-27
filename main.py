91a2b56d41465f129447fc6cefb6747974203b1d
Kei-141 <ruler141@gmail.com>
2021-09-27 23:29:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,29 +9,40 @@ from train import load_model
 from text import text_to_sequence
 from denoiser import Denoiser
 
-hparams = create_hparams()
-hparams.sampling_rate = 22050
+class MikochiSpeechSynthesis:
+    def __init__(self):
+        self.hparams = set_hparams()
+        self.model = set_model()
+        self.waveglow = set_waveglow()
+        
+    def set_hparams(self):
+        hparams = create_hparams()
+        hparams.sampling_rate = 22050
+        return hparams
+    
+    def set_model(self):
+        checkpoint_path = "/content/MikochiSpeechSynthesis/model/ver_1_0_0"
+        model = load_model(self.hparams)
+        model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
+        _ = model.cuda().eval().half()
+        return 
 
-checkpoint_path = "/content/MikochiSpeechSynthesis/model/ver_1_0_0"
-model = load_model(hparams)
-model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
-_ = model.cuda().eval().half()
-
-waveglow_path = 'waveglow_256channels_universal_v5.pt'
-waveglow = torch.load(waveglow_path)['model']
-waveglow.cuda().eval().half()
-for k in waveglow.convinv:
-    k.float()
-denoiser = Denoiser(waveglow)
-
-phones = input("テキストを入力してください。")
-sequence = np.array(text_to_sequence(phones, ['basic_cleaners']))[None, :]
-sequence = torch.autograd.Variable(
-    torch.from_numpy(sequence)).cuda().long()
-
-mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
-
-with torch.no_grad():
-    audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
-
-ipd.Audio(audio[0].data.cpu().numpy(), rate=hparams.sampling_rate)
+    def set_waveglow(self):
+        waveglow_path = 'waveglow_256channels_universal_v5.pt'
+        waveglow = torch.load(waveglow_path)['model']
+        waveglow.cuda().eval().half()
+        for k in waveglow.convinv:
+            k.float()
+        denoiser = Denoiser(waveglow)
+        return waveglow
+    
+    def generate_voice(self, text):
+        sequence = np.array(text_to_sequence(text, ['basic_cleaners']))[None, :]
+        sequence = torch.autograd.Variable(
+            torch.from_numpy(sequence)).cuda().long()
+        mel_outputs, mel_outputs_postnet, _, alignments = self.model.inference(sequence)
+        with torch.no_grad():
+            audio = self.waveglow.infer(mel_outputs_postnet, sigma=0.666)
+        wav = audio[0].data.cpu().numpy()
+        fs = self.hparams.sampling_rate
+        return wav, fs
